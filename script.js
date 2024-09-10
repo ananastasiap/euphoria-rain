80378f77a11f7e8b25ad96c47fd16e171a539497
ananastasiap <anestheziia@gmail.com>
2024-09-10 18:55:21 +0300

    feat: script for rains and hearts/html and style

---
+++ script.js
@@ -0,0 +1,37 @@
+// words rain
+const words = ["эфория", "ещё эйфория", "тоже эйфорию", "ЭЙФОРИЯ"];
+const containerRain = document.getElementById("containerRain");
+
+const createWord = () => {
+  const wordElement = document.createElement("div");
+  wordElement.classList.add("word");
+  wordElement.innerText = words[Math.floor(Math.random() * words.length)];
+
+  wordElement.style.left = Math.random() * 100 + "vw";
+
+  wordElement.style.animationDuration = Math.random() * 3 + 2 + "s";
+
+  containerRain.appendChild(wordElement);
+
+  setTimeout(() => {
+    wordElement.remove();
+  }, 5000);
+};
+
+setInterval(createWord, 100);
+
+// heart-click
+document.addEventListener("click", function (e) {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤";
+
+  heart.style.left = e.pageX + "px";
+  heart.style.top = e.pageY + "px";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 2000);
+});
